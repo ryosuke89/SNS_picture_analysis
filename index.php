5ad4493136ce64b018b532b3094e9f2d0e3b8a03
ryosuke <ryosukekt89@gmail.com>
2015-12-11 13:46:57 +0900

    定数を使うように変更しました。

--- index.php
+++ index.php
@@ -11,16 +11,16 @@ require_once '../common/scriptUtil.php';
   <body>
     <h1><a href="<?php echo ROOT_URL ?>">SNS Photos</a></h1>
     <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
-    <form action="./sns.php" method="POST">
+    <form action="<?php echo SNS; ?>" method="POST">
       SNS<br><br>
-      <a href="sns.php">Twitter</a><br>
-      <a href="sns.php">Facebook</a><br>
-      <a href="sns.php">Google+</a><br><br><br>
+      <a href="<?php echo SNS; ?>">Twitter</a><br>
+      <a href="<?php echo SNS; ?>">Facebook</a><br>
+      <a href="<?php echo SNS; ?>">Google+</a><br><br><br>
 
       カテゴリー<br><br>
-      <a href="category_detail.php">動物</a><br>
-      <a href="category_detail.php">風景</a><br>
-      <a href="category_detail.php">料理</a><br><br><br>
+      <a href="<?php echo CATEGORY_DETAIL; ?>">動物</a><br>
+      <a href="<?php echo CATEGORY_DETAIL; ?>">風景</a><br>
+      <a href="<?php echo CATEGORY_DETAIL; ?>">料理</a><br><br><br>
 
       サブサービス<br><br>
       サブサービス1<br>
@@ -29,7 +29,7 @@ require_once '../common/scriptUtil.php';
 
       <!--トップページへのリンク-->
       <?php echo return_top(); ?><br>
-      <a href="contact.php">お問い合わせ</a>
+      <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
     </form>
   </body>
 </html>
