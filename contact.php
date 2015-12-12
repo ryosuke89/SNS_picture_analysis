10f46dc8b381f9e9890be3b5253ca6a90ad964fb
ryosuke <ryosukekt89@gmail.com>
2015-12-12 18:00:18 +0900

    contact.phpを追加しました。

--- contact.php
+++ contact.php
@@ -5,34 +5,41 @@ require_once '../common/scriptUtil.php';
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="UTF-8">
-      <title>SNS Photos</title>
+  <meta charset="UTF-8">
+  <title>SNS Photos</title>
 </head>
   <body>
     <h1><a href="<?php echo ROOT_URL ?>">SNS Photos</a></h1>
     <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
     <form action="<?php echo CONTACT_CONFIRM; ?>" method="POST">
+      <table>
+        <tr>
+          <td>お名前：</td>
+          <td><input type="text" name="name"></td>
+        </tr><br><br>
 
-      お名前：　　　　　　　　　　
-      <input type="text" name="name">
-      <br><br>
+        <tr>
+          <td>メールアドレス：</td>
+          <td><input type="text" name="mail_address"></td>
+        </tr><br><br>
 
-      メールアドレス：　　　　　
-      <input type="text" name="address">
-      <br><br>
+        <tr>
+          <td>お問い合わせの種類：</td>
+          <td><select name="contact_kind">
+            <option value="" selected>--選択してください--</option>
+            <option value="使い方">使い方</option>
+            <option value="その他">その他</option>
+          </select></td><br><br>
+        </tr>
 
-      お問い合わせの種類：　
-      <select name="contact_kind">
-        <option value="" selected>--選択してください--</option>
-        <option value="使い方">使い方</option>
-        <option value="その他">その他</option>
-      </select><br><br>
-
-      お問い合わせ内容：　　
-      <textarea name="contact_contents" rows=10 cols=50 style="resize:none" wrap="hard"></textarea><br><br><br>
-        　　　　　　　　　　　　　　　　<input type="submit" name="NO" value="入力内容を確認する"><br>
-      <?php echo return_top(); ?><br>
-      <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
+        <tr><td>お問い合わせ内容：
+          <td><textarea name="contact_contents" rows=10 cols=50 style="resize:none" wrap="hard"></textarea></td><br><br><br>
+        </tr>
+      </table>
+      <input type="submit" name="confirm" value="入力内容を確認する"><br>
     </form>
+
+    <?php echo return_top(); ?><br>
+    <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
   </body>
 </html>
