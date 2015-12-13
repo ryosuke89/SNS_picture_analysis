18ef66b83c1cb4ccd0c4e120fef094c22948ecea
ryosuke <ryosukekt89@gmail.com>
2015-12-13 17:52:29 +0900

    円グラフを追加しました。

--- index.php
+++ index.php
@@ -7,10 +7,46 @@ require_once '../common/scriptUtil.php';
 <head>
   <meta charset="UTF-8">
   <title>SNS Photos</title>
+  <!-- AJAX API の読み込み -->
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript">
+
+    // 円グラフパッケージとVisualization APIの読み込み
+    google.load('visualization', '1.0', {'packages':['corechart']});
+
+    // Visualization APIを呼び出したときに実行するコールバックの設定
+    google.setOnLoadCallback(drawChart);
+
+    // データテーブルの作成と挿入のコールバック。
+    // 円グラフを作成し、データを渡し、描画する
+    function drawChart() {
+
+      // データテーブルの作成
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'SNS全体');
+      data.addColumn('number', '割合');
+      data.addRows([
+        ['動物', 40],
+        ['風景', 30],
+        ['その他', 30]
+      ]);
+
+      // チャートオプションの設定
+      var options = {'title':'SNS全体の割合',
+                     'width':600,
+                     'height':400};
+
+      // いくつかのオプションを渡してチャートを描画する
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
+  </script>
 </head>
   <body>
     <h1><a href="<?php echo ROOT_URL ?>">SNS Photos</a></h1>
     <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
+    <!--円グラフの表示-->
+    <div id="chart_div"></div>
     <form action="<?php echo SNS; ?>" method="POST">
       SNS<br><br>
       <a href="<?php echo SNS; ?>">Twitter</a><br>
