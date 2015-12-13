82ac19166157c003b5ac7f1f57b348264ebdc667
ryosuke <ryosukekt89@gmail.com>
2015-12-13 18:19:31 +0900

    セッションを取得する関数を追加しました。

--- scriptUtil.php
+++ scriptUtil.php
@@ -6,6 +6,15 @@ function return_top(){
     return "<a href='".ROOT_URL."'>トップページ</a>";
 }
 
+//セッションの値を取得する関数
+function contact_session($name){
+    if(isset($_POST['mode']) && $_POST['mode'] == 'REINPUT'){
+        if(isset($_SESSION[$name])){
+            return $_SESSION[$name];
+        }
+    }
+}
+
 //POSTの値をセッションに格納する関数
 function confirm_session($name){
     if(!empty($_POST[$name])){
