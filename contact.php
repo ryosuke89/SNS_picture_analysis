dc6d94afff529ff74a7e01667e9db2a8d57f34d3
ryosuke <ryosukekt89@gmail.com>
2015-12-13 18:18:48 +0900

    セッションを追加しました。

--- contact.php
+++ contact.php
@@ -12,28 +12,30 @@ require_once '../common/scriptUtil.php';
     <h1><a href="<?php echo ROOT_URL ?>">SNS Photos</a></h1>
     <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
     <form action="<?php echo CONTACT_CONFIRM; ?>" method="POST">
+      <?php session_start(); ?>
       <table>
         <tr>
           <td>お名前：</td>
-          <td><input type="text" name="name"></td>
+          <td><input type="text" name="name" value="<?php echo contact_session('name'); ?>"></td>
         </tr><br><br>
 
         <tr>
           <td>メールアドレス：</td>
-          <td><input type="text" name="mail_address"></td>
+          <td><input type="text" name="mail_address" value="<?php echo contact_session('mail_address'); ?>"></td>
         </tr><br><br>
 
         <tr>
           <td>お問い合わせの種類：</td>
           <td><select name="contact_kind">
-            <option value="" selected>--選択してください--</option>
-            <option value="使い方">使い方</option>
-            <option value="その他">その他</option>
+            <option value="">--選択してください--</option>
+            <option value="使い方" <?php if(contact_session('contact_kind') == "使い方"){echo "selected";} ?>>使い方</option>
+            <option value="問題点・改善点" <?php if(contact_session('contact_kind') == "問題点・改善点"){echo "selected";} ?>>問題点・改善点</option>
+            <option value="その他" <?php if(contact_session('contact_kind') == "その他"){echo "selected";} ?>>その他</option>
           </select></td><br><br>
         </tr>
 
         <tr><td>お問い合わせ内容：
-          <td><textarea name="contact_contents" rows=10 cols=50 style="resize:none" wrap="hard"></textarea></td><br><br><br>
+          <td><textarea name="contact_contents" rows=10 cols=50 style="resize:none" wrap="hard"><?php echo contact_session('contact_contents'); ?></textarea></td><br><br><br>
         </tr>
       </table>
       <input type="submit" name="confirm" value="入力内容を確認する"><br>
