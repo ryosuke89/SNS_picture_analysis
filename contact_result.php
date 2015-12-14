cefc7ba130a0ad97919019cf221b02d693366376
ryosuke <ryosukekt89@gmail.com>
2015-12-14 11:39:09 +0900

    URLで直接アクセスした場合の処理の追加。

--- contact_result.php
+++ contact_result.php
@@ -13,15 +13,22 @@ require_once '../common/scriptUtil.php';
     <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
 
     <?php
-    session_start();
-    echo 'お名前：' . $_SESSION['name'] . "<br>";
-    echo 'メールアドレス：' . $_SESSION['mail_address'] . "<br>";
-    echo 'お問い合わせの種類：' . $_SESSION['contact_kind'] . "<br>";
-    echo 'お問い合わせ内容：' . $_SESSION['contact_contents'] . "<br>" . "<br>";
-    ?>
-    上記の内容で送信しました。<br><br>
+    //URLで直接アクセスした場合のエラー処理
+    if(empty($_POST['mode'])){
+        echo 'アクセスルートが不正です。最初からやり直してください。<br>';
+    }elseif($_POST['mode'] == 'RESULT'){
+        ?>
+        <h3>送信結果</h3><br>
+        <?php
+        session_start();
+        echo 'お名前：' . $_SESSION['name'] . "<br>";
+        echo 'メールアドレス：' . $_SESSION['mail_address'] . "<br>";
+        echo 'お問い合わせの種類：' . $_SESSION['contact_kind'] . "<br>";
+        echo 'お問い合わせ内容：' . $_SESSION['contact_contents'] . "<br>" . "<br>";
+        echo '上記の内容で送信しました。<br>';
+    }
 
-    <?php echo return_top(); ?><br>
+    echo return_top(); ?><br>
     <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
   </body>
 </html>
