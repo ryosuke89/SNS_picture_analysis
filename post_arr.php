86e327ffb9bb4210d1857e1f677ea78d61e92ddd
ryosuke <ryosukekt89@gmail.com>
2015-12-17 00:13:54 +0900

    post_arr.phpを追加しました。

--- post_arr.php
+++ post_arr.php
@@ -6,20 +6,24 @@
   <body>
     <?php
     $url = 'https://gateway.watsonplatform.net/visual-recognition-beta/api/v1/tag/recognize';
-$data = array(
-	'username' => 'da6f5e0b-334d-4889-92b4-bcf4e013658a',
-	'password' => 'YzXGqvlZJ83d'
-);
-$header = Array(
-	"Content-Type: application/x-www-form-urlencoded"
-);
-$options = array('http' => array(
-	'method' => 'POST',
-	'header'  => implode("\r\n", $header),
-	'content' => http_build_query($data),
-));
-$contents = file_get_contents($url, false, stream_context_create($options));
-echo $contents;
+    $data = array(
+	                'username' => 'da6f5e0b-334d-4889-92b4-bcf4e013658a',
+                  'password' => 'YzXGqvlZJ83d'
+    );
+
+    $header = array(
+                    'Content-Type: application/x-www-form-urlencoded',
+                    sprintf('Authorization: Basic %s', base64_encode(sprintf('%s:%s', $data['username'], $data['password']))) // IDとPASSをbase64エンコードして放り込む
+    );
+
+    $options = array('http' => array(
+	                                   'method' => 'POST',
+                                     'header'  => implode("\r\n", $header),
+                                     'content' => http_build_query($data),
+    ));
+
+    $contents = file_get_contents($url, false, stream_context_create($options));
+    echo $contents;
     ?>
   </body>
 </html>
