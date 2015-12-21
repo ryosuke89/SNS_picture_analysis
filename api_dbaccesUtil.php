60c55848ce8f5c35f5af7f741fda1bff89eba707
ryosuke <ryosukekt89@gmail.com>
2015-12-21 22:24:51 +0900

    photoテーブルを表示する関数の追加。

--- api_dbaccesUtil.php
+++ api_dbaccesUtil.php
@@ -11,28 +11,45 @@ function connect_MySQL(){
     }
 }
 
-//画像のURLと投稿日をDBに取得する関数
-function insert_photo($photoURL, $postedDate, $snsID){
+//画像のURLと投稿日時をDBに取得する関数
+function insert_photo($photoURL, $postedDatetime, $snsID){
 
     $insert_db = connect_MySQL();
 
-    $insert_sql = "INSERT INTO photo(photoURL,postedDate,snsID)"
-            . "VALUES(:photoURL,:postedDate,:snsID)";
+    $insert_sql = "INSERT INTO photo(photoURL,postedDatetime,snsID)"
+            . "VALUES(:photoURL,:postedDatetime,:snsID)";
 
     $insert_query = $insert_db->prepare($insert_sql);
 
     $insert_query->bindValue(':photoURL',$photoURL);
-    $insert_query->bindValue(':postedDate',$postedDate);
+    $insert_query->bindValue(':postedDatetime',$postedDatetime);
     $insert_query->bindValue(':snsID',$snsID);
 
     try{
         $insert_query->execute();
     } catch (PDOException $e) {
-        //接続オブジェクトを初期化することでDB接続を切断
         $insert_db=null;
         return $e->getMessage();
     }
 
     $insert_db=null;
     return null;
+}
+
+//画像のURLと投稿日時を表示する関数
+function select_photo(){
+
+    $select_db = connect_MySQL();
+    $select_sql = "SELECT * FROM photo";
+    $select_query = $select_db->prepare($select_sql);
+
+    try{
+        $select_query->execute();
+    } catch (PDOException $e) {
+        $select_query=null;
+        return $e->getMessage();
+    }
+
+    //該当するレコードを連想配列として返却
+    return $select_query->fetchAll(PDO::FETCH_ASSOC);
 }
