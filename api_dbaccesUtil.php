e0e8726f21bd9ae28ba400953bb33e332480db2e
ryosuke <ryosukekt89@gmail.com>
2015-12-25 00:47:09 +0900

    画像認識の結果に関する関数の追加。

--- api_dbaccesUtil.php
+++ api_dbaccesUtil.php
@@ -34,11 +34,102 @@ function insert_photo($photoURL, $postedDatetime, $snsID){
     return null;
 }
 
+//種類の確認用に画像認識の結果を取得する関数
+function insert_kind_s($kind_s, $category_s){
+
+    $sample_db = connect_MySQL();
+    $sample_sql = "INSERT INTO recognition(kind_s, category_s)"
+                      . "VALUES(:kind_s,:category_s)";
+    $sample_query = $sample_db->prepare($sample_sql);
+
+    $sample_query->bindValue(':kind_s',$kind_s);
+    $sample_query->bindValue(':category_s',$category_s);
+
+    try{
+        $sample_query->execute();
+    } catch (PDOException $e) {
+        $sample_db=null;
+        return $e->getMessage();
+    }
+
+    $sample_db=null;
+    return null;
+}
+
+//画像認識の結果を取得する関数
+function insert_recognition($result1, $result2, $result3, $result4, $result5,
+            $result6, $result7, $result8, $result9, $result10,
+            $result11, $result12, $result13, $result14, $result15,
+            $result16, $result17, $result18, $result19, $result20, $snsID, $photoID){
+
+    $insert_recognition_db = connect_MySQL();
+    $insert_recognition_sql = "INSERT INTO recognition(result1,result2,result3,
+            result4,result5,result6,result7,result8,result9,result10,
+            result11,result12,result13,result14,result15,
+            result16,result17,result18,result19,result20,snsID,photoID)"
+            . "VALUES(:result1,:result2,:result3,:result4,:result5,
+            :result6,:result7,:result8,:result9,:result10,
+            :result11,:result12,:result13,:result14,:result15,
+            :result16,:result17,:result18,:result19,:result20,:snsID,:photoID)";
+    $insert_recognition_query = $insert_recognition_db->prepare($insert_recognition_sql);
+
+    $insert_recognition_query->bindValue(':result1',$result1);
+    $insert_recognition_query->bindValue(':result2',$result2);
+    $insert_recognition_query->bindValue(':result3',$result3);
+    $insert_recognition_query->bindValue(':result4',$result4);
+    $insert_recognition_query->bindValue(':result5',$result5);
+    $insert_recognition_query->bindValue(':result6',$result6);
+    $insert_recognition_query->bindValue(':result7',$result7);
+    $insert_recognition_query->bindValue(':result8',$result8);
+    $insert_recognition_query->bindValue(':result9',$result9);
+    $insert_recognition_query->bindValue(':result10',$result10);
+    $insert_recognition_query->bindValue(':result11',$result11);
+    $insert_recognition_query->bindValue(':result12',$result12);
+    $insert_recognition_query->bindValue(':result13',$result13);
+    $insert_recognition_query->bindValue(':result14',$result14);
+    $insert_recognition_query->bindValue(':result15',$result15);
+    $insert_recognition_query->bindValue(':result16',$result16);
+    $insert_recognition_query->bindValue(':result17',$result17);
+    $insert_recognition_query->bindValue(':result18',$result18);
+    $insert_recognition_query->bindValue(':result19',$result19);
+    $insert_recognition_query->bindValue(':result20',$result20);
+    $insert_recognition_query->bindValue(':snsID',$snsID);
+    $insert_recognition_query->bindValue(':photoID',$photoID);
+
+    try{
+        $insert_recognition_query->execute();
+    } catch (PDOException $e) {
+        $insert_recognition_db=null;
+        return $e->getMessage();
+    }
+
+    $insert_recognition_db=null;
+    return null;
+}
+
 //画像のURLと投稿日時を表示する関数
 function select_photo(){
 
     $select_db = connect_MySQL();
     $select_sql = "SELECT * FROM photo";
+    $select_query = $select_db->prepare($select_sql);
+
+    try{
+        $select_query->execute();
+    } catch (PDOException $e) {
+        $select_query=null;
+        return $e->getMessage();
+    }
+
+    //該当するレコードを連想配列として返却
+    return $select_query->fetchAll(PDO::FETCH_ASSOC);
+}
+
+//画像認識の結果を表示する関数
+function select_recognition(){
+
+    $select_db = connect_MySQL();
+    $select_sql = "SELECT * FROM recognition";
     $select_query = $select_db->prepare($select_sql);
 
     try{
