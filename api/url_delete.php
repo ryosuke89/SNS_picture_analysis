a8f00f8644ce638a06c4f587b59b3325f787aa0d
ryosuke <ryosukekt89@gmail.com>
2015-12-28 14:51:28 +0900

    url_delete.phpを追加しました。

---
+++ api/url_delete.php
@@ -0,0 +1,24 @@
+﻿<?php require_once 'api_dbaccesUtil.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+</head>
+  <body>
+      <?php
+      //削除する範囲のphotoIDを入力
+      $first = 1000;
+      $last = 2000;
+
+      //不要な画像のURLと投稿日時を削除する
+      for($i = $first; $i <= $last; $i++){
+          $result = delete_photo($first);
+          $first = $first + 1;
+      }
+      if(!isset($result)){
+          echo '削除しました。';
+      }
+      ?>
+    </table>
+  </body>
+</html>
