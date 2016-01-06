f588510a6758c45afc78a5f34144b64f03f8199a
ryosuke <ryosukekt89@gmail.com>
2016-01-06 16:27:32 +0900

    contact_result.phpを追加しました。

---
+++ app/contact_result.php
@@ -0,0 +1,34 @@
+<?php
+require_once '../common/defineUtil.php';
+require_once '../common/scriptUtil.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>SNS Photos</title>
+</head>
+  <body>
+    <h1><a href="<?php echo ROOT_URL; ?>">SNS Photos</a></h1>
+    <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
+
+    <?php
+    //URLで直接アクセスした場合のエラー処理
+    if(empty($_POST['mode'])){
+        echo 'アクセスルートが不正です。最初からやり直してください。<br>';
+    }elseif($_POST['mode'] == 'RESULT'){
+        ?>
+        <h3>送信結果</h3><br>
+        <?php
+        session_start();
+        echo 'お名前：' . $_SESSION['name'] . "<br>";
+        echo 'メールアドレス：' . $_SESSION['mail_address'] . "<br>";
+        echo 'お問い合わせの種類：' . $_SESSION['contact_kind'] . "<br>";
+        echo 'お問い合わせ内容：' . $_SESSION['contact_contents'] . "<br>" . "<br>";
+        echo '上記の内容で送信しました。<br>';
+    }
+
+    echo return_top(); ?><br>
+    <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
+  </body>
+</html>
