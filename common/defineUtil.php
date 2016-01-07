54de6d780bf1e581acdb09d4e6c6f7824f0aa0a8
ryosuke <ryosukekt89@gmail.com>
2016-01-07 14:19:11 +0900

    defineUtil.phpを追加しました。

---
+++ common/defineUtil.php
@@ -0,0 +1,8 @@
+<?php
+const ROOT_URL = 'http://localhost/develop/sns_photos/app';   //indexディレクトリへのURL
+const TOP_URI = '/';                                          //トップページ
+const SNS = 'sns.php';                                        //SNSページ
+const CATEGORY_DETAIL = 'category_detail.php';                //カテゴリー／詳細ページ
+const CONTACT = 'contact.php';                                //お問い合わせページ
+const CONTACT_CONFIRM = 'contact_confirm.php';                //お問い合わせ確認ページ
+const CONTACT_RESULT = 'contact_result.php';                  //お問い合わせ結果ページ
