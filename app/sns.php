0b302ce477b479738f62e185257f6a18d2faae7b
ryosuke <ryosukekt89@gmail.com>
2016-01-11 17:14:29 +0900

    画像をランダムで表示する処理の追加。

--- app/sns.php
+++ app/sns.php
@@ -1,56 +1,24 @@
-<?php
+﻿<?php
 require_once '../common/defineUtil.php';
 require_once '../common/scriptUtil.php';
 require_once '../common/dbaccesUtil.php';
 
 //categoryテーブルの値を割合が高い順に取得
-$result_category = select_sns_category(ex_sns($_GET['sns']));
+$result_category = select_category(ex_sns($_GET['sns']));
 //kindテーブルの値を割合が高い順に取得
-$result_kind = select_sns_kind(ex_sns($_GET['sns']));
+$result_kind = select_kind(ex_sns($_GET['sns']));
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <title>SNS Photos</title>
-  <!-- AJAX API の読み込み -->
+  <!--AJAX APIの読み込み-->
   <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript" src="scriptUtil.php"></script>
   <script type="text/javascript">
-
-    // 円グラフパッケージとVisualization APIの読み込み
-    google.load('visualization', '1.0', {'packages':['corechart']});
-
-    // Visualization APIを呼び出したときに実行するコールバックの設定
-    google.setOnLoadCallback(drawChart);
-
-    // データテーブルの作成と挿入のコールバック。
-    // 円グラフを作成し、データを渡し、描画する
-    function drawChart() {
-
-      // データテーブルの作成
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'カテゴリー');
-      data.addColumn('number', '割合');
-      //円グラフにSNSの割合を表示する処理
-      <?php
-      foreach($result_category as $value_category){
-      ?>
-      data.addRows([
-        ['<?php echo $value_category['categoryName']; ?>', <?php echo $value_category['categoryPercentage']; ?>]
-      ]);
-      <?php
-      }
-      ?>
-
-      // チャートオプションの設定
-      var options = {'title':'<?php if($_GET['sns'] == 'Google '){echo 'Google+';}else{echo $_GET['sns'];} ?>の割合',
-                     'width':600,
-                     'height':400};
-
-      // いくつかのオプションを渡してチャートを描画する
-      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+  //SNSごとの割合の円グラフを表示する処理の取得
+  <?php chart($_GET['sns'], $result_category); ?>
   </script>
 </head>
   <body>
@@ -60,6 +28,9 @@ $result_kind = select_sns_kind(ex_sns($_GET['sns']));
     <div id="chart_div"></div>
     <form action="<?php echo SNS; ?>" method="POST">
       <?php
+      //配列、配列番号の初期化
+      $url_array = array();
+      $key = 0;
       //カテゴリーごとの種類の割合をテーブル型で表示
       foreach($result_category as $value_category){
           ?>
@@ -80,18 +51,30 @@ $result_kind = select_sns_kind(ex_sns($_GET['sns']));
                     <?php
                 }
             }
+
             //カテゴリーごとの画像の番号を取得
             $result_photoID = photoID_calc(ex_sns($_GET['sns']), $value_category['categoryName']);
             //カテゴリーごとの画像のURLを取得
             foreach($result_photoID as $value_photoID){
                 $result_url = url_photo($value_photoID['photoID']);
-                //画像の表示
+                //画像のURLを配列に格納
                 foreach($result_url as $value_url){
-                    ?>
-                    <img src="<?php echo $value_url['photoURL']; ?>" width="80" height="80"/>
-                    <?php
+                    $url_array += array($key=>$value_url['photoURL']);
+                    $key = $key + 1;
                 }
             }
+
+            //画像をランダムで表示
+            shuffle($url_array);
+            for($i = 0; $i < 6; $i++){
+                ?>
+                <img src="<?php echo $url_array[$i]; ?>" width="100" height="100"/>
+                <?php
+            }
+
+            //配列、配列番号の初期化
+            $url_array = array();
+            $key = 0;
             ?>
           </table><br>
           <?php
