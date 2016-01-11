50071f8ce3af8d8d19efce8356c87863a182cae9
ryosuke <ryosukekt89@gmail.com>
2016-01-11 17:12:03 +0900

    円グラフの表示処理をscriptUtil.phpに移動。

--- app/index.php
+++ app/index.php
@@ -4,51 +4,19 @@ require_once '../common/scriptUtil.php';
 require_once '../common/dbaccesUtil.php';
 
 //categoryテーブルの値を割合が高い順に取得
-$result_category = select_all_category();
+$result_category = select_category(5);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <title>SNS Photos</title>
-  <!-- AJAX API の読み込み -->
+  <!--AJAX APIの読み込み-->
   <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript" src="scriptUtil.php"></script>
   <script type="text/javascript">
-
-    // 円グラフパッケージとVisualization APIの読み込み
-    google.load('visualization', '1.0', {'packages':['corechart']});
-
-    // Visualization APIを呼び出したときに実行するコールバックの設定
-    google.setOnLoadCallback(drawChart);
-
-    // データテーブルの作成と挿入のコールバック。
-    // 円グラフを作成し、データを渡し、描画する
-    function drawChart() {
-
-      // データテーブルの作成
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'SNS全体');
-      data.addColumn('number', '割合');
-      //円グラフにSNS全体の割合を表示する処理
-      <?php
-      foreach($result_category as $value_category){
-      ?>
-      data.addRows([
-        ['<?php echo $value_category['categoryName']; ?>', <?php echo $value_category['categoryPercentage']; ?>]
-      ]);
-      <?php
-      }
-      ?>
-
-      // チャートオプションの設定
-      var options = {'title':'SNS全体の割合',
-                     'width':600,
-                     'height':400};
-
-      // いくつかのオプションを渡してチャートを描画する
-      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+  //SNS全体の割合の円グラフを表示する処理の取得
+  <?php chart(null, $result_category); ?>
   </script>
 </head>
   <body>
@@ -61,21 +29,18 @@ $result_category = select_all_category();
       SNS<br><br>
       <a href="<?php echo SNS; ?>?sns=Twitter">Twitter</a><br>
       <a href="<?php echo SNS; ?>?sns=Facebook">Facebook</a><br>
-      <a href="<?php echo SNS; ?>?sns=Google+">Google+</a><br><br><br>
+      Google+<br>
+      Instagram<br><br><br>
 
       カテゴリー<br><br>
-      <a href="<?php echo CATEGORY_DETAIL; ?>?category=風景">風景</a><br>
-      <a href="<?php echo CATEGORY_DETAIL; ?>?category=物品">物品</a><br>
-      <a href="<?php echo CATEGORY_DETAIL; ?>?category=人物">人物</a><br>
-      <a href="<?php echo CATEGORY_DETAIL; ?>?category=絵・図">絵・図</a><br>
-      <a href="<?php echo CATEGORY_DETAIL; ?>?category=動物">動物</a><br>
-      <a href="<?php echo CATEGORY_DETAIL; ?>?category=食品・料理">食品・料理</a><br>
-      <a href="<?php echo CATEGORY_DETAIL; ?>?category=乗り物">乗り物</a><br><br><br>
-
-      サブサービス<br><br>
-      サブサービス1<br>
-      サブサービス2<br>
-      サブサービス3<br><br>
+      <?php
+      foreach($result_category as $value_category){
+          ?>
+          <a href="<?php echo CATEGORY_DETAIL; ?>?category=<?php echo $value_category['categoryName']; ?>"><?php echo $value_category['categoryName']; ?></a><br>
+          <?php
+      }
+      ?>
+      <br><br>
     </form>
 
     <!--フッター-->
