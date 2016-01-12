b3f728f5f3a31424a4869b6d4c7f8bcc38e9b7ae
ryosuke <ryosukekt89@gmail.com>
2016-01-12 17:03:05 +0900

    カテゴリーごとの割合の円グラフを表示する関数を変更。

--- common/scriptUtil.php
+++ common/scriptUtil.php
@@ -81,7 +81,12 @@ function chart($get_sns, $result_category){
 }
 
 //カテゴリーごとの割合の円グラフを表示する関数
-function detail_chart($get_sns, $get_category, $result_kind){
+function detail_chart($get_sns, $get_category, $kind_array){
+    //フラグの初期化
+    $flag_TWT = false;
+    $flag_FB = false;
+    $flag_GGP = false;
+    $flag_INS = false;
     ?>
     //円グラフパッケージとVisualization APIの読み込み
     google.load('visualization', '1.0', {'packages':['corechart']});
@@ -93,89 +98,143 @@ function detail_chart($get_sns, $get_category, $result_kind){
     //円グラフを作成し、データを渡し、描画する
     function drawChart(){
 
-        //データテーブルの作成
-        var data1 = new google.visualization.DataTable();
-        data1.addColumn('string', 'Twitterでの種類');
-        data1.addColumn('number', '割合');
-        //円グラフに種類の割合を表示する処理
         <?php
-        foreach($result_kind as $value_kind){
+        if(empty($get_sns) || $get_sns == TWT){
             ?>
-            data1.addRows([
-                ['<?php echo $value_kind['kindName']; ?>', <?php echo $value_kind['kindPercentage']; ?>]
-            ]);
+            //データテーブルの作成
+            var data1 = new google.visualization.DataTable();
+            data1.addColumn('string', 'Twitterでの種類');
+            data1.addColumn('number', '割合');
+            //円グラフに種類の割合を表示する処理
+            <?php
+            foreach($kind_array as $value_kind_array){
+                if($value_kind_array['snsID'] == 1){
+                    ?>
+                    data1.addRows([
+                        ['<?php echo $value_kind_array['kindName']; ?>', <?php echo $value_kind_array['kindPercentage']; ?>]
+                    ]);
+                    <?php
+                    $flag_TWT = true;
+                }
+            }
+        }
+
+        if(empty($get_sns) || $get_sns == FB){
+            ?>
+            var data2 = new google.visualization.DataTable();
+            data2.addColumn('string', 'Facebookでの種類');
+            data2.addColumn('number', '割合');
+            <?php
+            foreach($kind_array as $value_kind_array){
+                if($value_kind_array['snsID'] == 2){
+                    ?>
+                    data2.addRows([
+                        ['<?php echo $value_kind_array['kindName']; ?>', <?php echo $value_kind_array['kindPercentage']; ?>]
+                    ]);
+                    <?php
+                    $flag_FB = true;
+                }
+            }
+        }
+
+        if(empty($get_sns) || $get_sns == 'Google '){
+            ?>
+            var data3 = new google.visualization.DataTable();
+            data3.addColumn('string', 'Google+での種類');
+            data3.addColumn('number', '割合');
+            <?php
+            foreach($kind_array as $value_kind_array){
+                if($value_kind_array['snsID'] == 3){
+                    ?>
+                    data3.addRows([
+                        ['<?php echo $value_kind_array['kindName']; ?>', <?php echo $value_kind_array['kindPercentage']; ?>]
+                    ]);
+                    <?php
+                    $flag_GGP = true;
+                }
+            }
+        }
+
+        if(empty($get_sns) || $get_sns == INS){
+            ?>
+            var data4 = new google.visualization.DataTable();
+            data4.addColumn('string', 'Instagramでの種類');
+            data4.addColumn('number', '割合');
+            <?php
+            foreach($kind_array as $value_kind_array){
+                if($value_kind_array['snsID'] == 4){
+                    ?>
+                    data4.addRows([
+                        ['<?php echo $value_kind_array['kindName']; ?>', <?php echo $value_kind_array['kindPercentage']; ?>]
+                    ]);
+                    <?php
+                    $flag_INS = true;
+                }
+            }
+        }
+
+        if($flag_TWT == true){
+            ?>
+            //チャートオプションの設定
+            var options1 = {'title':'<?php echo TWT . 'での' . $get_category; ?>の割合',
+                            'width':600,
+                            'height':400};
+            <?php
+        }
+
+        if($flag_FB == true){
+            ?>
+            var options2 = {'title':'<?php echo FB . 'での' . $get_category; ?>の割合',
+                            'width':600,
+                            'height':400};
+            <?php
+        }
+
+        if($flag_GGP == true){
+            ?>
+            var options3 = {'title':'<?php echo GGP . 'での' . $get_category; ?>の割合',
+                            'width':600,
+                            'height':400};
+            <?php
+        }
+
+        if($flag_INS == true){
+            ?>
+            var options4 = {'title':'<?php echo INS . 'での' . $get_category; ?>の割合',
+                            'width':600,
+                            'height':400};
+            <?php
+        }
+
+        if($flag_TWT == true){
+            ?>
+            //いくつかのオプションを渡してチャートを描画する
+            var chart1 = new google.visualization.PieChart(document.getElementById('chart_div1'));
+            chart1.draw(data1, options1);
+            <?php
+        }
+
+        if($flag_FB == true){
+            ?>
+            var chart2 = new google.visualization.PieChart(document.getElementById('chart_div2'));
+            chart2.draw(data2, options2);
+            <?php
+        }
+
+        if($flag_GGP == true){
+            ?>
+            var chart3 = new google.visualization.PieChart(document.getElementById('chart_div3'));
+            chart3.draw(data3, options3);
+            <?php
+        }
+
+        if($flag_INS == true){
+            ?>
+            var chart4 = new google.visualization.PieChart(document.getElementById('chart_div4'));
+            chart4.draw(data4, options4);
             <?php
         }
         ?>
-
-        var data2 = new google.visualization.DataTable();
-        data2.addColumn('string', 'Facebookでの種類');
-        data2.addColumn('number', '割合');
-        <?php
-        foreach($result_kind as $value_kind){
-            ?>
-            data2.addRows([
-                ['<?php echo $value_kind['kindName']; ?>', <?php echo $value_kind['kindPercentage']; ?>]
-            ]);
-            <?php
-        }
-        ?>
-
-        var data3 = new google.visualization.DataTable();
-        data3.addColumn('string', 'Google+での種類');
-        data3.addColumn('number', '割合');
-        <?php
-        foreach($result_kind as $value_kind){
-            ?>
-            data3.addRows([
-                ['<?php echo $value_kind['kindName']; ?>', <?php echo $value_kind['kindPercentage']; ?>]
-            ]);
-            <?php
-        }
-        ?>
-
-        var data4 = new google.visualization.DataTable();
-        data4.addColumn('string', 'Instagramでの種類');
-        data4.addColumn('number', '割合');
-        <?php
-        foreach($result_kind as $value_kind){
-            ?>
-            data4.addRows([
-                ['<?php echo $value_kind['kindName']; ?>', <?php echo $value_kind['kindPercentage']; ?>]
-            ]);
-            <?php
-        }
-        ?>
-
-        //チャートオプションの設定
-        var options1 = {'title':'<?php if(empty($get_sns)){echo 'Twitterでの' . $get_category; }else{echo $get_sns . 'での' . $get_category; } ?>の割合',
-                        'width':600,
-                        'height':400};
-
-        var options2 = {'title':'<?php if(empty($get_sns)){echo 'Twitterでの' . $get_category; }else{echo $get_sns . 'での' . $get_category; } ?>の割合',
-                        'width':600,
-                        'height':400};
-
-        var options3 = {'title':'<?php if(empty($get_sns)){echo 'Twitterでの' . $get_category; }else{echo $get_sns . 'での' . $get_category; } ?>の割合',
-                        'width':600,
-                        'height':400};
-
-        var options4 = {'title':'<?php if(empty($get_sns)){echo 'Twitterでの' . $get_category; }else{echo $get_sns . 'での' . $get_category; } ?>の割合',
-                        'width':600,
-                        'height':400};
-
-        //いくつかのオプションを渡してチャートを描画する
-        var chart1 = new google.visualization.PieChart(document.getElementById('chart_div1'));
-        chart1.draw(data1, options1);
-
-        var chart2 = new google.visualization.PieChart(document.getElementById('chart_div2'));
-        chart2.draw(data2, options2);
-
-        var chart3 = new google.visualization.PieChart(document.getElementById('chart_div3'));
-        chart3.draw(data3, options3);
-
-        var chart4 = new google.visualization.PieChart(document.getElementById('chart_div4'));
-        chart4.draw(data4, options4);
     }
     <?php
 }
