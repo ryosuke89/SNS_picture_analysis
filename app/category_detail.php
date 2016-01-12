3e5d7893e40a292b04631af840d2eece7e55e46a
ryosuke <ryosukekt89@gmail.com>
2016-01-12 17:02:04 +0900

    円グラフの表示処理をscriptUtil.phpに移動。

--- app/category_detail.php
+++ app/category_detail.php
@@ -5,59 +5,49 @@ require_once '../common/dbaccesUtil.php';
 
 //categoryテーブルの値を取得
 $result_category = select_detail_category($_GET['category']);
+
+//配列、配列番号の初期化
+$kind_array = array();
+$key = 0;
+
 //kindテーブルの値を割合が高い順に取得
-$result_kind = select_detail_kind($result_category[0]['categoryID']);
+foreach($result_category as $value_category){
+    $result_kind = select_detail_kind($value_category['categoryID']);
+    //全ての配列を結合
+    foreach($result_kind as $value_kind){
+        $kind_array += array($key=>$value_kind);
+        $key = $key + 1;
+    }
+}
+
+//GETの取得
+if(empty($_GET['sns'])){
+    $sns = null;
+}else{
+    $sns = $_GET['sns'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <title>SNS Photos</title>
-  <!-- AJAX API の読み込み -->
+  <!--AJAX APIの読み込み-->
   <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript" src="scriptUtil.php"></script>
   <script type="text/javascript">
-
-    // 円グラフパッケージとVisualization APIの読み込み
-    google.load('visualization', '1.0', {'packages':['corechart']});
-
-    // Visualization APIを呼び出したときに実行するコールバックの設定
-    google.setOnLoadCallback(drawChart);
-
-    // データテーブルの作成と挿入のコールバック。
-    // 円グラフを作成し、データを渡し、描画する
-    function drawChart() {
-
-      // データテーブルの作成
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'SNSでの種類');
-      data.addColumn('number', '割合');
-      //円グラフに種類の割合を表示する処理
-      <?php
-      foreach($result_kind as $value_kind){
-      ?>
-      data.addRows([
-        ['<?php echo $value_kind['kindName']; ?>', <?php echo $value_kind['kindPercentage']; ?>]
-      ]);
-      <?php
-      }
-      ?>
-
-      // チャートオプションの設定
-      var options = {'title':'<?php if(empty($_GET['sns'])){echo 'Twitterでの' . $_GET['category']; }else{echo $_GET['sns'] . 'での' . $_GET['category']; } ?>の割合',
-                     'width':600,
-                     'height':400};
-
-      // いくつかのオプションを渡してチャートを描画する
-      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+  //カテゴリーごとの割合の円グラフを表示する処理の取得
+  <?php detail_chart($sns, $_GET['category'], $kind_array); ?>
   </script>
 </head>
   <body>
     <h1><a href="<?php echo ROOT_URL; ?>">SNS Photos</a></h1>
     <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
     <!--円グラフの表示-->
-    <div id="chart_div"></div>
+    <div id="chart_div1"></div>
+    <div id="chart_div2"></div>
+    <div id="chart_div3"></div>
+    <div id="chart_div4"></div>
     <form action="<?php echo SNS; ?>" method="POST">
       <table border=1>
         <tr>
@@ -91,7 +81,7 @@ $result_kind = select_detail_kind($result_category[0]['categoryID']);
                 //画像の表示
                 foreach($result_url as $value_url){
                     ?>
-                    <img src="<?php echo $value_url['photoURL']; ?>" width="80" height="80"/>
+                    <img src="<?php echo $value_url['photoURL']; ?>" width="100" height="100"/>
                     <?php
                 }
             }
