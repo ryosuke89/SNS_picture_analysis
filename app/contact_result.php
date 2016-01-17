e41426fb895d1bf2b30d17ec4e9a704c81b6cd02
ryosuke <ryosukekt89@gmail.com>
2016-01-17 16:05:59 +0900

    CSSの追加。

--- app/contact_result.php
+++ app/contact_result.php
@@ -7,28 +7,40 @@ require_once '../common/scriptUtil.php';
 <head>
   <meta charset="UTF-8">
   <title>SNS Photos</title>
+  <link rel="stylesheet" type="text/css" href="../css/css_contact_result.css">
 </head>
   <body>
-    <h1><a href="<?php echo ROOT_URL; ?>">SNS Photos</a></h1>
-    <h4>SNSに投稿されている画像の傾向分析サイト</h4><br>
-
+    <div id="header"><div class="title"><a href="<?php echo ROOT_URL; ?>">
+    <span style="vertical-align: middle; font-size: 35px;">SNS Photos</span></a>
+    <span style="vertical-align: 3px; font-size: 50%; margin-left: 20px">
+    SNSに投稿されている画像の傾向分析サイト</span></div></div>
+    <div id="content">
     <?php
-    //URLで直接アクセスした場合のエラー処理
+    //URLで直接アクセスした場合、トップページに移動する処理
     if(empty($_POST['mode'])){
-        echo 'アクセスルートが不正です。最初からやり直してください。<br>';
+        ?>
+        <meta http-equiv="refresh" content="0; URL=<?php echo ROOT_URL; ?>">
+        <?php
     }elseif($_POST['mode'] == 'RESULT'){
         ?>
-        <h3>送信結果</h3><br>
+        <div class="text">送信結果</div>
         <?php
         session_start();
         echo 'お名前：' . $_SESSION['name'] . "<br>";
         echo 'メールアドレス：' . $_SESSION['mail_address'] . "<br>";
         echo 'お問い合わせの種類：' . $_SESSION['contact_kind'] . "<br>";
-        echo 'お問い合わせ内容：' . $_SESSION['contact_contents'] . "<br>" . "<br>";
-        echo '上記の内容で送信しました。<br>';
+        echo 'お問い合わせ内容：' . $_SESSION['contact_contents'];
+        ?>
+        <div class="result">
+        上記の内容で送信しました。</div>
+        <?php
     }
-
-    echo return_top(); ?><br>
-    <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
+    ?>
+    </div>
+    <div id="footer">
+    <div class="link">
+    <?php echo return_top(); ?>
+    <span style="margin-left: 63px">
+    <a href="<?php echo CONTACT; ?>">お問い合わせ</a></span></div></div>
   </body>
 </html>
