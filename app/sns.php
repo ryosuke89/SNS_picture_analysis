51623f12ecc877221b662155e5cb7234c9b3074b
ryosuke <ryosukekt89@gmail.com>
2016-01-22 14:24:32 +0900

    インデントの修正。

--- app/sns.php
+++ app/sns.php
@@ -6,8 +6,11 @@ require_once '../common/dbaccesUtil.php';
 //エラーを表示させない処理
 if(empty($_GET['sns'])){
     $_GET['sns'] = null;
+}
+
+//GETを受け取れない、またはGETで受け取ったものが存在しない場合、トップページに移動する処理
+if($_GET['sns'] != TWT && $_GET['sns'] != FB && $_GET['sns'] != 'Google ' && $_GET['sns'] != INS){
     ?>
-    <!--GETを受け取れない場合、トップページに移動する処理-->
     <meta http-equiv="refresh" content="0; URL=<?php echo ROOT_URL; ?>">
     <?php
 }
@@ -32,82 +35,99 @@ $result_kind = select_kind(ex_sns($_GET['sns']));
   </script>
 </head>
   <body>
-    <div id="header"><div class="title"><a href="<?php echo ROOT_URL; ?>">
-    <span style="vertical-align: middle; font-size: 35px;">SNS Photos</span></a>
-    <span style="vertical-align: 3px; font-size: 50%; margin-left: 20px">
-    SNSに投稿されている画像の傾向分析サイト</span></div></div>
+    <!--ヘッダー-->
+    <div id="header">
+      <div class="title">
+        <a href="<?php echo ROOT_URL; ?>">
+          <span style="vertical-align: middle; font-size: 35px;">SNS Photos</span>
+        </a>
+        <span style="vertical-align: 3px; font-size: 50%; margin-left: 20px">
+          SNSに投稿されている画像の傾向分析サイト
+        </span>
+      </div>
+    </div>
+
     <div id="content">
-    <!--円グラフの表示-->
-    <div id="chart_div"></div>
-    <form action="<?php echo SNS; ?>" method="POST">
-      <?php
-      //配列、配列番号の初期化
-      $url_array = array();
-      $key = 0;
-      ?>
-      <div class="category">
-      <?php
-      //カテゴリーごとの種類の割合をテーブル型で表示
-      foreach($result_category as $value_category){
-          ?>
-          <a href="<?php echo CATEGORY_DETAIL; ?>?sns=<?php echo $_GET['sns']; ?>&category=<?php echo $value_category['categoryName']; ?>">
-          <span style="font-size: 20px;"><?php echo $value_category['categoryName']; ?></a>の割合：<?php echo $value_category['categoryPercentage']; ?>％</span>
-          <table class="table">
-            <tr>
-              <th>種類</th>
-              <th>割合</th>
-            </tr>
-            <?php
-            foreach($result_kind as $value_kind){
-                if($value_category['categoryID'] == $value_kind['categoryID']){
-                    ?>
-                    <tr>
-                      <td><?php echo $value_kind['kindName']; ?></td>
-                      <td><?php echo $value_kind['kindPercentage']; ?>％</td>
-                    </tr>
-                    <?php
-                }
-            }
-            ?>
-          </table>
-          <div class="photo">
-            <?php
-            //カテゴリーごとの画像の番号を取得
-            $result_photoID = photoID_calc(ex_sns($_GET['sns']), $value_category['categoryName']);
-            //カテゴリーごとの画像のURLを取得
-            foreach($result_photoID as $value_photoID){
-                $result_url = url_photo($value_photoID['photoID']);
-                //画像のURLを配列に格納
-                foreach($result_url as $value_url){
-                    $url_array += array($key=>$value_url['photoURL']);
-                    $key = $key + 1;
-                }
-            }
+      <!--円グラフの表示-->
+      <div id="chart_div"></div>
+      <form action="<?php echo SNS; ?>" method="POST">
+        <?php
+        //配列、配列番号の初期化
+        $url_array = array();
+        $key = 0;
 
-            //重複する画像のURLを配列から削除
-            $url_unique = array_unique($url_array);
-            //画像のURLをランダムにする処理
-            shuffle($url_unique);
-            //画像の表示
-            for($i = 0; $i < 4; $i++){
-                ?>
-                <img src="<?php echo $url_unique[$i]; ?>" width="150" height="150"/>
-                <?php
-            }
-
-            //配列、配列番号の初期化
-            $url_array = array();
-            $key = 0;
+        //カテゴリーごとの種類の割合をテーブル型で表示
+        foreach($result_category as $value_category){
             ?>
+            <div class="category">
+              <a href="<?php echo CATEGORY_DETAIL; ?>?sns=<?php echo $_GET['sns']; ?>&category=<?php echo $value_category['categoryName']; ?>">
+                <?php echo $value_category['categoryName']; ?></a>の割合：
+              <?php echo $value_category['categoryPercentage']; ?>％
+            </div>
+
+            <table class="table">
+              <tr>
+                <th>種類</th>
+                <th>割合</th>
+              </tr>
+              <?php
+              foreach($result_kind as $value_kind){
+                  if($value_category['categoryID'] == $value_kind['categoryID']){
+                      ?>
+                      <tr>
+                        <td><?php echo $value_kind['kindName']; ?></td>
+                        <td><?php echo $value_kind['kindPercentage']; ?>％</td>
+                      </tr>
+                      <?php
+                  }
+              }
+              ?>
+            </table>
+
+            <div class="photo">
+              <?php
+              //カテゴリーごとの画像の番号を取得
+              $result_photoID = photoID_calc(ex_sns($_GET['sns']), $value_category['categoryName']);
+              //カテゴリーごとの画像のURLを取得
+              foreach($result_photoID as $value_photoID){
+                  $result_url = url_photo($value_photoID['photoID']);
+                  //画像のURLを配列に格納
+                  foreach($result_url as $value_url){
+                      $url_array += array($key=>$value_url['photoURL']);
+                      $key = $key + 1;
+                  }
+              }
+
+              //重複する画像のURLを配列から削除
+              $url_unique = array_unique($url_array);
+              //画像のURLをランダムにする処理
+              shuffle($url_unique);
+              //画像の表示
+              for($i = 0; $i < 4; $i++){
+                  ?>
+                  <img src="<?php echo $url_unique[$i]; ?>" width="150" height="150"/>
+                  <?php
+              }
+
+              //配列、配列番号の初期化
+              $url_array = array();
+              $key = 0;
+              ?>
             </div>
             <?php
-      }
-      ?>
-    </form></div>
+        }
+        ?>
+      </form>
+    </div>
+
+    <!--フッター-->
     <div id="footer">
-    <div class="link">
-    <?php echo return_top(); ?>
-    <span style="margin-left: 63px">
-    <a href="<?php echo CONTACT; ?>">お問い合わせ</a></span></div></div>
+      <div class="link">
+        <?php echo return_top(); ?>
+        <span style="margin-left: 63px">
+          <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
+        </span>
+      </div>
+    </div>
   </body>
 </html>
