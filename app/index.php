94682aad6696d579c111652d365f1f913fa525aa
ryosuke <ryosukekt89@gmail.com>
2016-01-22 14:24:24 +0900

    インデントの修正。

--- app/index.php
+++ app/index.php
@@ -22,40 +22,51 @@ $result_category = select_category(5);
 </head>
   <body>
     <!--ヘッダー-->
-    <div id="header"><div class="title"><a href="<?php echo ROOT_URL; ?>">
-    <span style="vertical-align: middle; font-size: 35px;">SNS Photos</span></a>
-    <span style="vertical-align: 3px; font-size: 50%; margin-left: 20px">
-    SNSに投稿されている画像の傾向分析サイト</span></div></div>
-    <div id="content">
-    <!--円グラフの表示-->
-    <div id="chart_div"></div>
-    <form action="<?php echo SNS; ?>" method="POST">
-      <div class="sns">
-      <div class="text">
-      SNS</div>
-      <a href="<?php echo SNS; ?>?sns=Twitter">Twitter</a><br>
-      Facebook<br>
-      Google+<br>
-      Instagram</div>
+    <div id="header">
+      <div class="title">
+        <a href="<?php echo ROOT_URL; ?>">
+          <span style="vertical-align: middle; font-size: 35px;">SNS Photos</span>
+        </a>
+        <span style="vertical-align: 3px; font-size: 50%; margin-left: 20px">
+          SNSに投稿されている画像の傾向分析サイト
+        </span>
+      </div>
+    </div>
 
-      <div class="category">
-      <div class="text">
-      カテゴリー</div>
-      <?php
-      foreach($result_category as $value_category){
-          ?>
-          <a href="<?php echo CATEGORY_DETAIL; ?>?category=<?php echo $value_category['categoryName']; ?>"><?php echo $value_category['categoryName']; ?></a><br>
+    <div id="content">
+      <!--円グラフの表示-->
+      <div id="chart_div"></div>
+      <form action="<?php echo SNS; ?>" method="POST">
+        <div class="sns">
+          <div class="text">SNS</div>
+          <a href="<?php echo SNS; ?>?sns=Twitter">Twitter</a><br>
+          Facebook<br>
+          Google+<br>
+          Instagram
+        </div>
+
+        <div class="category">
+          <div class="text">カテゴリー</div>
           <?php
-      }
-      ?>
-      <br><br></div>
-    </form></div>
+          foreach($result_category as $value_category){
+              ?>
+              <a href="<?php echo CATEGORY_DETAIL; ?>?category=<?php echo $value_category['categoryName']; ?>"><?php echo $value_category['categoryName']; ?></a><br>
+              <?php
+          }
+          ?>
+          <br><br>
+        </div>
+      </form>
+    </div>
 
     <!--フッター-->
     <div id="footer">
-    <div class="link">
-    <?php echo return_top(); ?>
-    <span style="margin-left: 63px">
-    <a href="<?php echo CONTACT; ?>">お問い合わせ</a></span></div></div>
+      <div class="link">
+        <?php echo return_top(); ?>
+        <span style="margin-left: 63px">
+          <a href="<?php echo CONTACT; ?>">お問い合わせ</a>
+        </span>
+      </div>
+    </div>
   </body>
 </html>
