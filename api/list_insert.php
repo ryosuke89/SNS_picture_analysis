2d0c08c74cab27c2234e06b2e5df8cea52e548cf
ryosuke <ryosukekt89@gmail.com>
2016-01-26 10:42:16 +0900

    リストに存在しない場合のみ入力フォームを表示するように変更。

--- api/list_insert.php
+++ api/list_insert.php
@@ -19,44 +19,34 @@
       </tr>
 
       <?php
-      //認識結果1と入力フォームを表示
+      //認識結果1と一致するものがリストに存在するかを確認
       foreach($select_recognition as $value_recognition){
-          ?>
-          <form action="./list_insert.php" method="POST">
-            <tr>
-              <td><input type="text" name="listName" value="<?php echo $value_recognition['result1']; ?>"></td>
-              <td><input type="text" name="listKind" value="<?php foreach($select_list as $value_list){if($value_recognition['result1'] == $value_list['listName']){echo $value_list['listKind'];}} ?>"></td>
-              <td><input type="text" name="listCategory" value="<?php foreach($select_list as $value_list){if($value_recognition['result1'] == $value_list['listName']){echo $value_list['listCategory'];}} ?>"></td>
-              <td><input type="submit" name="btnSubmit" style="width:40px" <?php foreach($select_list as $value_list){if($value_recognition['result1'] == $value_list['listName']){ ?>value=""<?php }} ?>></td>
-            </tr>
-          </form>
-          <?php
+          $flag = false;
+          foreach($select_list as $value_list){
+              if($value_recognition['result1'] == $value_list['listName']){
+                  $flag = true;
+              }
+          }
+          //リストに存在しない場合、認識結果1と入力フォームを表示
+          if($flag == false){
+              ?>
+              <form action="./list_insert.php" method="POST">
+                <tr>
+                  <td><input type="text" name="listName" value="<?php echo $value_recognition['result1']; ?>"></td>
+                  <td><input type="text" name="listKind"></td>
+                  <td><input type="text" name="listCategory"></td>
+                  <td><input type="submit" name="btnSubmit"></td>
+                </tr>
+              </form>
+              <?php
+          }
       }
       ?>
     </table>
     <?php
-    //POSTの取得
-    if(empty($_POST['listName'])){
-        $listName = null;
-    }else{
-        $listName = $_POST['listName'];
-    }
-
-    if(empty($_POST['listKind'])){
-        $listKind = null;
-    }else{
-        $listKind = $_POST['listKind'];
-    }
-
-    if(empty($_POST['listCategory'])){
-        $listCategory = null;
-    }else{
-        $listCategory = $_POST['listCategory'];
-    }
-
     //リストをDBに追加
-    if(!empty($listName) && !empty($listKind) && !empty($listCategory)){
-        $insert_result = insert_list($listName, $listKind, $listCategory);
+    if(!empty($_POST['listName']) && !empty($_POST['listKind']) && !empty($_POST['listCategory'])){
+        $insert_result = insert_list($_POST['listName'], $_POST['listKind'], $_POST['listCategory']);
     }
     ?>
   </body>
